bd4cb651fe4f6b1dd35cadc6849a4a287d9136a8
winches <329487092@qq.com>
2022-11-03 15:44:53 +0800

    refactor: git-cz style change

--- .cz-config.js
+++ .cz-config.js
@@ -1,46 +1,77 @@
-'use strict'
 module.exports = {
+  // type 类型（定义之后，可通过上下键选择）
   types: [
-    { value: '✨新增', name: '新增:    新的内容' },
-    { value: '🐛修复', name: '修复:    修复一个Bug' },
-    { value: '📝文档', name: '文档:    变更的只有文档' },
-    { value: '💄格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '♻️重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '⚡️性能', name: '性能:    提升性能' },
-    { value: '✅测试', name: '测试:    添加一个测试' },
-    { value: '🔧工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '⏪回滚', name: '回滚:    代码回退' }
+    { value: 'feat', name: '✨新增:    新的内容' },
+    { value: 'fix', name: '🐛修复:    修复一个Bug' },
+    { value: 'docs', name: '📝文档:    变更的只有文档' },
+    { value: 'style', name: '💄格式:    空格, 分号等格式修复' },
+    { value: 'refactor', name: '💡重构:    代码重构，注意和特性、修复区分开' },
+    { value: 'pref', name: '🌩性能:    提升性能' },
+    { value: 'test', name: '✅测试:    添加一个测试' },
+    { value: 'chore', name: '🔧工具:    开发工具变动(构建、脚手架工具等)' },
+    { value: 'revert', name: '⏪回滚:    代码回退' }
   ],
+
+  // scope 类型（定义之后，可通过上下键选择）
   scopes: [
-    { name: 'leetcode' },
-    { name: 'javascript' },
-    { name: 'typescript' },
-    { name: 'Vue' },
-    { name: 'node' }
-  ],
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },  */
-  // override the messages, defaults are as follows
+    ['components', '组件相关'],
+    ['hooks', 'hook 相关'],
+    ['utils', 'utils 相关'],
+    ['ui', '对 ui 的调整'],
+    ['styles', '样式相关'],
+    ['deps', '项目依赖'],
+    ['auth', '对 auth 修改'],
+    ['other', '其他修改'],
+    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
+    ['custom', '以上都不是？我要自定义']
+  ].map(([value, description]) => {
+    return {
+      value,
+      name: `${value.padEnd(30)} (${description})`
+    }
+  }),
+
+  // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
+  // allowCustomScopes: true,
+
+  // allowTicketNumber: false,
+  // isTicketNumberRequired: false,
+  // ticketNumberPrefix: 'TICKET-',
+  // ticketNumberRegExp: '\\d{1,5}',
+
+  // 针对每一个 type 去定义对应的 scopes，例如 fix
+  /*
+    scopeOverrides: {
+      fix: [
+        { name: 'merge' },
+        { name: 'style' },
+        { name: 'e2eTest' },
+        { name: 'unitTest' }
+      ]
+    },
+    */
+
+  // 交互提示信息
   messages: {
-    type: '选择一种你的提交类型:',
-    scope: '选择一个scope (可选):',
-    // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: '短说明:\n',
-    body: '长说明，使用"|"换行(可选)：\n',
-    breaking: '非兼容性说明 (可选):\n',
-    footer: '关联关闭的issue，例如：#31, #34(可选):\n',
-    confirmCommit: '确定提交说明?(yes/no)'
+    type: '确保本次提交遵循：前端代码规范！\n选择你要提交的类型：',
+    scope: '\n选择一个 scope（可选）：',
+    // 选择 scope: custom 时会出下面的提示
+    customScope: '请输入自定义的 scope：',
+    subject: '填写简短精炼的变更描述：\n',
+    body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
+    breaking: '列举非兼容性重大的变更（可选）：\n',
+    footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
+    confirmCommit: '确认提交？'
   },
-  allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
-  // limit subject length
-  subjectLimit: 100
+
+  // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
+  allowBreakingChanges: ['feat', 'fix'],
+
+  // 跳过要询问的步骤
+  skipQuestions: ['scope', 'body', 'breaking', 'footer'],
+
+  subjectLimit: 100, // subject 限制长度
+  breaklineChar: '|' // 换行符，支持 body 和 footer
+  // footerPrefix : 'ISSUES CLOSED:'
+  // askForBreakingChangeFirst : true,
 }
