a8ac445581312fbb4fabfa1352ce042055b598eb
winches <329487092@qq.com>
2022-11-06 17:21:53 +0800

    feat: add idux logo

--- histoire.config.ts
+++ histoire.config.ts
@@ -1,7 +1,19 @@
+import path from 'path'
 import { defineConfig } from 'histoire'
 import { HstVue } from '@histoire/plugin-vue'
+
+const iduxLogo = path.resolve(__dirname, './src/assets/logo/logo.svg')
 
 export default defineConfig({
   setupFile: './src/histoire.setup.ts',
   plugins: [HstVue()],
+  theme: {
+    title: 'Idux Interactive Docs',
+    logo: {
+      dark: iduxLogo,
+      light: iduxLogo,
+      square: iduxLogo,
+    },
+    favicon: './src/assets/logo/favicon.ico',
+  },
 })
