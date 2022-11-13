d3a188961fda66d5aad085e1b8c4a3f2b9e46d30
winches <329487092@qq.com>
2022-11-13 12:13:05 +0800

    update: button add more display

--- src/types/hst-components.d.ts
+++ src/types/hst-components.d.ts
@@ -1,4 +1,4 @@
-import { HstRadio } from '@histoire/controls'
+import { HstRadio, HstSelect } from '@histoire/controls'
 import '@vue/runtime-core'
 
 export {}
@@ -6,5 +6,6 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     HstRadio: typeof HstRadio
+    HstSelect: typeof HstSelect
   }
 }
