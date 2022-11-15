1fb18b90be7b2cf12ad6bc560171b064a98e9e74
winches <329487092@qq.com>
2022-11-15 15:28:39 +0800

    update: optimize divider display

--- src/components/basicComponents/layout/Divider/divider.story.vue
+++ src/components/basicComponents/layout/Divider/divider.story.vue
@@ -110,9 +110,9 @@ const dividerSizeOptions: HstControlOption<DividerProps['size']>[] = [
         <Describe>
           使用 plain 可以设置为更轻量的分割线文字样式。
         </Describe>
-        <IxDivider :label="state.label" />
-        <p>见山是山，见水是水；见山不是山，见水不是水；见山还是山，见水还是水。</p>
         <IxDivider :label="state.label" :plain="state.plain" />
+        <p>见山是山，见水是水；见山不是山，见水不是水；见山还是山，见水还是水。</p>
+        <IxDivider :label="state.label" />
         <p>见山是山，见水是水；见山不是山，见水不是水；见山还是山，见水还是水。</p>
       </template>
 
